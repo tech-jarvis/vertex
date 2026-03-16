3068bacc529576d20360e6e3d027f6cd22ae3e4f
Jarvis <mail.asharali@gmail.com>
2026-03-16 07:47:15 -0700

    Updated

--- api/sitemap.js
+++ api/sitemap.js
@@ -4,85 +4,85 @@ const SITEMAP = `<?xml version="1.0" encoding="UTF-8"?>
         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
         xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
   <url>
-    <loc>https://vertexbdcltd.com/</loc>
+    <loc>https://vertexbdcllc.com/</loc>
     <lastmod>2026-03-16</lastmod>
     <changefreq>weekly</changefreq>
     <priority>1.0</priority>
   </url>
   <url>
-    <loc>https://vertexbdcltd.com/automotive-bdc</loc>
+    <loc>https://vertexbdcllc.com/automotive-bdc</loc>
     <lastmod>2026-03-16</lastmod>
     <changefreq>weekly</changefreq>
     <priority>0.95</priority>
   </url>
   <url>
-    <loc>https://vertexbdcltd.com/services</loc>
+    <loc>https://vertexbdcllc.com/services</loc>
     <lastmod>2026-03-12</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.9</priority>
   </url>
   <url>
-    <loc>https://vertexbdcltd.com/contact</loc>
+    <loc>https://vertexbdcllc.com/contact</loc>
     <lastmod>2026-03-12</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.9</priority>
   </url>
   <url>
-    <loc>https://vertexbdcltd.com/why-us</loc>
+    <loc>https://vertexbdcllc.com/why-us</loc>
     <lastmod>2026-03-12</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.85</priority>
   </url>
   <url>
-    <loc>https://vertexbdcltd.com/solar-bdc</loc>
+    <loc>https://vertexbdcllc.com/solar-bdc</loc>
     <lastmod>2026-03-12</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.8</priority>
   </url>
   <url>
-    <loc>https://vertexbdcltd.com/rv-bdc</loc>
+    <loc>https://vertexbdcllc.com/rv-bdc</loc>
     <lastmod>2026-03-12</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.8</priority>
   </url>
   <url>
-    <loc>https://vertexbdcltd.com/roofing-bdc</loc>
+    <loc>https://vertexbdcllc.com/roofing-bdc</loc>
     <lastmod>2026-03-12</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.8</priority>
   </url>
   <url>
-    <loc>https://vertexbdcltd.com/real-estate-bdc</loc>
+    <loc>https://vertexbdcllc.com/real-estate-bdc</loc>
     <lastmod>2026-03-12</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.8</priority>
   </url>
   <url>
-    <loc>https://vertexbdcltd.com/saas-bdc</loc>
+    <loc>https://vertexbdcllc.com/saas-bdc</loc>
     <lastmod>2026-03-12</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.8</priority>
   </url>
   <url>
-    <loc>https://vertexbdcltd.com/logistics-bdc</loc>
+    <loc>https://vertexbdcllc.com/logistics-bdc</loc>
     <lastmod>2026-03-12</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.8</priority>
   </url>
   <url>
-    <loc>https://vertexbdcltd.com/financial-bdc</loc>
+    <loc>https://vertexbdcllc.com/financial-bdc</loc>
     <lastmod>2026-03-12</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.8</priority>
   </url>
   <url>
-    <loc>https://vertexbdcltd.com/tech-stack</loc>
+    <loc>https://vertexbdcllc.com/tech-stack</loc>
     <lastmod>2026-03-12</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.8</priority>
   </url>
   <url>
-    <loc>https://vertexbdcltd.com/careers</loc>
+    <loc>https://vertexbdcllc.com/careers</loc>
     <lastmod>2026-03-12</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.8</priority>
